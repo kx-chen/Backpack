356271ee0b989c62edef02afd98e6cae28c9c578
Kai Chen <kaichen120@gmail.com>
2020-04-09 00:00:08 -0700

    VO-1 First iteration of sidebar

--- app/Root.js
+++ app/Root.js
@@ -3,9 +3,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { hot } from 'react-hot-loader/root';
-import type { Store } from '../reducers/types';
-import App from './App';
-import {Route, Router, Switch} from "react-router";
+import type { Store } from './reducers/types';
+import App from './containers/App';
 
 type Props = {
   store: Store,
---
+++ app/containers/NavContainer.js
@@ -0,0 +1,4 @@
+import Nav from "../components/Nav";
+import { connect } from 'react-redux';
+
+export default connect()(Nav);
