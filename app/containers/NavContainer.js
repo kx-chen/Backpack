f9ef0c8947857d7bac8de147bb64a39f2588388f
Kai Chen <kaichen120@gmail.com>
2020-04-10 00:28:53 -0700

    RO-9 Add sidebar arrow to open/close menu
    Remove old reducers, etc.

--- app/containers/NavContainer.js
+++ app/containers/NavContainer.js
@@ -1,16 +1,28 @@
 import { connect } from 'react-redux';
 import Nav from '../components/Nav';
+<<<<<<< HEAD
 import { toggleMenu } from "../actions/sidenav";
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
     onMenuChange: (open) => dispatch(toggleMenu(open))
+=======
+import { toggleMenu } from '../actions/sidenav';
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onMenuChange: open => dispatch(toggleMenu(open))
+>>>>>>> RO-9 Add sidebar arrow to open/close menu
   };
 }
 
 function mapStateToProps(state, ownProps) {
   return {
+<<<<<<< HEAD
     open: state.sidenav.open,
+=======
+    open: state.sidenav.open
+>>>>>>> RO-9 Add sidebar arrow to open/close menu
   };
 }
 
