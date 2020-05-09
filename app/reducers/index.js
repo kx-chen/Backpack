57b900727adb4b5045384a99cf03d9e5f6e2a11a
Kai Chen <kaichen120@gmail.com>
2020-05-09 14:24:33 -0700

    RO-72 Remove Flow

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,13 +1,11 @@
-// @flow
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import type { HashHistory } from 'history';
 import posts from './post';
 import sidenav from './sidenav';
 import subreddits from './subreddits';
 
-export default function createRootReducer(history: HashHistory) {
-  return combineReducers<{}, *>({
+export default function createRootReducer(history) {
+  return combineReducers({
     router: connectRouter(history),
     posts,
     sidenav,
